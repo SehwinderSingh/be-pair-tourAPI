a6a7d7d881634a785637d0c032fdd5c6bc0ae500
Anubhav <anubhav675@gmail.com>
2026-08-31 21:57:16 +0300

    chore(tours): list all tours

--- tourLib.js
+++ tourLib.js
@@ -8,18 +8,26 @@ const addOne = (name, info, image, price, location) => {
 };
 
 const getAll = () => tours;
-module.exports = { addOne, getAll };
-
-
 
 if (require.main === module) {
-  const result = addOne(
-    "7 Days Tour",
-    "Join us for the Best of Helsinki!",
-    "https://www.course-api.com/images/tours/tour-x.jpeg",
-    "1,495",
+  addOne(
+    "Paris in 7 Days",
+    "Explore Paris.",
+    "https://example.com/paris.jpg",
+    "1,995",
+    "Paris, France"
+  );
+
+  addOne(
+    "Helsinki Weekend",
+    "Discover Helsinki.",
+    "https://example.com/helsinki.jpg",
+    "795",
     "Helsinki, Finland"
   );
 
-  console.log(result);
+  console.log(getAll());
 }
+
+
+module.exports = { addOne, getAll };
